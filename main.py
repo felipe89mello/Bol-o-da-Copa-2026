2225bed24feb5b9ba9aa32ca599b4e29ab272277
felipe89mello <felipe89mello@gmail.com>
2026-05-20 16:37:47 -0300

    removendo uvicorn local

--- main.py
+++ main.py
@@ -802,12 +802,3 @@ def ver_palpites_usuario(
     }
  
  
-if __name__ == "__main__":
-    import uvicorn
- 
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
-        reload=True
-    )
